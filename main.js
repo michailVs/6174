4f43c1148f1e58c21666bb011e59d7dbd0e843d8
MichailVs <mcohotnik21@gmail.com>
2023-07-01 22:29:47 +0300

    add "Enter" keydown on input

--- main.js
+++ main.js
@@ -1,54 +1,62 @@
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
-const output = document.querySelector('.output')
-const n = document.querySelector('.num')
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+const n = document.querySelector(".num");
 
-function inputValid (input) {
-    try {
-        let a
-        if (+input.value.trim() / 1) {
-            a = input.value.trim().slice(0, 4)
-            n.innerText = `Your number: ${a}\n`
-            return a
-        }
-        throw new Error(n.innerText = `"${input.value}" is not a number`)
+function inputValid(input) {
+  try {
+    let a;
+    if (+input.value.trim() / 1) {
+      a = input.value.trim().slice(0, 4);
+      n.innerText = `Your number: ${a}\n`;
+      return a;
     }
-    catch (e) {
-        throw new Error(n.innerText = e)
-    }
+    throw new Error((n.innerText = `"${input.value}" is not a number`));
+  } catch (e) {
+    throw new Error((n.innerText = e));
+  }
 }
 
-function minNum (x) {
-    try {
-        let min = x.split('').sort((a, b) => a - b)
-        return min.join('')
-    } catch (e) {
-        throw new Error(n.innerText = e)
-    }
+function minNum(x) {
+  try {
+    let min = x.split("").sort((a, b) => a - b);
+    return min.join("");
+  } catch (e) {
+    throw new Error((n.innerText = e));
+  }
 }
-function maxNum (x) {
-    try {
-        let max = x.split('').sort((a, b) => b - a)
-        return max.join('')
-    }
-    catch (e) {
-        throw new Error(n.innerText = e)
-    }
-}
-
-function calc (num1, num2, step = 0) {
-    let count = step
-    if (+num2 - +num1 === 6174) {
-        count++
-        return output.innerText += `${count}. ${num2} - ${num1} = 6174`
-    }
-    count++
-    output.innerText += `${count}. ${num2} - ${num1} = ${+num2 - +num1}\n`
-    calc(minNum(String(+num2 - +num1)), maxNum(String(+num2 - +num1)), count)
+function maxNum(x) {
+  try {
+    let max = x.split("").sort((a, b) => b - a);
+    return max.join("");
+  } catch (e) {
+    throw new Error((n.innerText = e));
+  }
 }
 
-btn.addEventListener('click', () => {
-    output.innerText = ''
-    calc(minNum(inputValid(input)), maxNum(inputValid(input)))
-    input.value = ''
-})
+function calc(num1, num2, step = 0) {
+  let count = step;
+  if (+num2 - +num1 === 6174) {
+    count++;
+    return (output.innerText += `${count}. ${num2} - ${num1} = 6174`);
+  }
+  count++;
+  output.innerText += `${count}. ${num2} - ${num1} = ${+num2 - +num1}\n`;
+  calc(minNum(String(+num2 - +num1)), maxNum(String(+num2 - +num1)), count);
+}
+function move(input) {
+  output.innerText = "";
+  calc(minNum(inputValid(input)), maxNum(inputValid(input)));
+  input.value = "";
+}
+
+btn.addEventListener("click", () => {
+  move(input);
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13 && input.value.length !== 0) {
+    e.preventDefault();
+    move(input);
+  }
+});
